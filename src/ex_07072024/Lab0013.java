89ee3c2411166f7ee72f3429715283ffa4856517
Admin <tejapatil53@gmail.com>
2024-07-09 15:52:20 +0530

    All remaining files added

---
+++ src/ex_07072024/Lab0013.java
@@ -0,0 +1,11 @@
+package ex_07072024;
+
+public class Lab0013 {
+    // Can I run class without a main method?
+    // run option?
+    static public void main(String[] args) {
+    }
+
+    public static void main() {
+    }
+}
---
+++ src/ex_07072024/Lab012.java
@@ -0,0 +1,9 @@
+package ex_07072024;
+
+public class Lab012 {
+    public static void main(String [] abc)
+    {
+        System.out.println("Interview Q");
+    }
+
+}
