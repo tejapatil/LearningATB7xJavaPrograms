ecb144e957c36aa6a8a44804d9b5666e1b7fd430
Admin <tejapatil53@gmail.com>
2024-07-18 16:07:53 +0530

    All remaining files added

---
+++ src/ex_13072024/Lab027.java
@@ -0,0 +1,13 @@
+package ex_13072024;
+
+public class Lab027 {
+    public static void main(String args[])
+    {
+        int age = 10; //Decimal Number system
+        int b = 0b1010; // Binday - Automation x
+        int octal = 0101; //If your Automation you may be working with these numbers
+        int hex = 0Xface;
+
+        //https://www.rapidtables.com/convert/number/decimal-to-hex.html?x=10
+    }
+}
