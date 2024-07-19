0b5de8859a3da6e379c0170bc53a5fd76dfbc3b5
Admin <tejapatil53@gmail.com>
2024-07-19 11:35:29 +0530

    All remaining files added

---
+++ src/ex_13072024/Lab044.java
@@ -0,0 +1,11 @@
+package ex_13072024;
+
+public class Lab044 {
+    public static void main(String args[])
+    {
+        System.out.println(10 == 10);
+        System.out.println (10 == 10); //  10> 10 or 10 = 10
+        System.out.println( 10 <= 10); // 10 < 10 or 10 = 10
+        System.out.println( 10 > 10);
+        System.out.println( 10 < 10);    }
+}
