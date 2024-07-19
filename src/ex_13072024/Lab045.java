f6d3d22fe84d22ee8af8413d72474d63d59a32c9
Admin <tejapatil53@gmail.com>
2024-07-19 23:30:18 +0530

    All remaining files added

--- src/ex_13072024/Lab045.java
+++ src/ex_13072024/Lab045.java
@@ -2,10 +2,10 @@ package ex_13072024;
 
 public class Lab045 {
     public static void main(String args[]){
-        boolean a = true;
-        System.out.println(!a);
-        System.out.println(!(10>20));
-        System.out.println(!!!!(30>90));
-        //  System.out.println((30>90)!);
+   System.out.println(10 == 10); // == Compare true values
+   System.out.println(10 >= 10); //  10> 10 or 10 = 10
+   System.out.println(10 <= 10); // 10 < 10 or 10 = 10
+   System.out.println(10 > 10);
+   System.out.println(10 < 10);
     }
 }
