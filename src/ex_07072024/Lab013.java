7f47c7a68a767152fa08509e487b17bec746142b
Admin <tejapatil53@gmail.com>
2024-07-26 13:18:38 +0530

    All remaining files added

--- src/ex_07072024/Lab013.java
+++ src/ex_07072024/Lab013.java
@@ -1,6 +1,6 @@
 package ex_07072024;
 
-public class Lab0013 {
+public class Lab013 {
     // Can I run class without a main method?
     // run option?
     static public void main(String[] args) {
