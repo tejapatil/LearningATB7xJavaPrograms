a99c08ba2d942fde3740838590653990351c2031
Admin <tejapatil53@gmail.com>
2024-08-05 17:59:23 +0530

    All remaining files added

---
+++ src/ex_22072024/Lab139.java
@@ -0,0 +1,37 @@
+package ex_22072024;
+
+import java.util.Scanner;
+
+public class Lab139 {
+    public static void main(String[] args) {
+        //Arrays ->
+        int [] marks=new int[3];
+        // index - 0,1,2
+        // len - 3
+        System.out.println(marks[0]);
+        System.out.println(marks[1]);
+        System.out.println(marks[2]);
+        Scanner sc=new Scanner(System.in);
+        marks[0]= sc.nextInt();
+        marks[1]=92;
+        marks[2]=93;
+        System.out.println(marks[0]);
+        System.out.println(marks[1]);
+        System.out.println(marks[2]);
+//        System.out.println(marks[10]); // AIOBE
+
+
+        boolean[] is_married={true, false, true};
+        //to Print
+
+        for(int i=0; i< marks.length;i++){
+            //i=0,1,2
+            System.out.println(i +"->" + marks[i]);
+        }
+
+        String weekDays[]={"Sunday", "Monday", "Tuesday", "Wednesday"};
+        for (int i = 0; i < weekDays.length; i++) {
+            System.out.println(weekDays[i]);
+        }
+    }
+}
---
+++ src/ex_22072024/Lab152.java
@@ -0,0 +1,26 @@
+package ex_22072024;
+
+public class Lab152 {
+    public static void main(String[] args) {
+        //Strings immutable in nature.
+        // StringBuilder , StringBuffer
+
+        String s1="PRAMOD";
+        s1="Dutta";
+
+        StringBuffer stringBuffer=new StringBuffer("Shresth");
+        // change [the value of string - Buffer
+        stringBuffer.append("Hatti");//ShresthHatti
+        stringBuffer.append("JI"); //ShresthHatti
+
+        StringBuilder stringBuilder= new StringBuilder("Tejaswini");
+        stringBuilder.append("Patil");
+
+        // Thead Safty?
+
+        // stringBuilder - not thread safe - people LOVE it :)
+
+
+
+    }
+}
